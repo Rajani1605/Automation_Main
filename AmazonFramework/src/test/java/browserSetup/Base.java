e722b0455085df50baf238b0728815e5c9ed920c
rajan <rajan@LAPTOP-G3985CDU>
2022-12-05 17:38:22 +0530

    m-adding comments in Base class

--- AmazonFramework/src/test/java/browserSetup/Base.java
+++ AmazonFramework/src/test/java/browserSetup/Base.java
@@ -6,23 +6,22 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
-	
+	//For Chrome Browser
 	public static WebDriver openChromeBrowser() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajan\\Desktop\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
-	
+	//For Firefox Browser
 	public static WebDriver openFirefoxBrowser() {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\rajan\\Desktop\\Drivers\\geckodriver.exe");
 		WebDriver driver= new FirefoxDriver ();
 		return driver;
 	}
-
+	//For Edge Browser
 	public static WebDriver openEdgeBrowser() {
 		System.setProperty("webdriver.edge.driver", "C:\\Users\\rajan\\Desktop\\Drivers\\msedgedriver.exe");
 		WebDriver driver= new EdgeDriver ();
 		return driver;
 	}
-	
 }
